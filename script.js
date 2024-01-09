9800c5e84b4629d5e19508b67362122ef83c2733
Fatemeh <fatemeh.sheybani2000@gmail.com>
2024-01-10 00:22:53 +0330

    change header section and add media query in style.css

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 // Script for navigation bar
-let $ = document
-let mainImg = $.getElementById('mainImg')
-let smallImgs = $.querySelectorAll('.small-img')
+let $ = document;
+let mainImg = $.getElementById("mainImg");
+let smallImgs = $.querySelectorAll(".small-img");
+const bar = $.getElementById("bar");
+const nav = $.getElementById("navbar");
+const close = $.getElementById("close");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
 
 smallImgs.forEach((img) => {
-    img.addEventListener('click', () => {
-        mainImg.src = img.src
-    })
-
-})
+  img.addEventListener("click", () => {
+    mainImg.src = img.src;
+  });
+});
